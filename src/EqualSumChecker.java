0507340bac72b794f19512347f9d64fc9ca11b98
Devin Raleigh <devinkr@gmail.com>
2023-02-24 15:26:04 -0700

    Exercise #7

---
+++ src/EqualSumChecker.java
@@ -0,0 +1,10 @@
+public class EqualSumChecker {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean hasEqualSum(int a, int b, int sum) {
+        return (a + b == sum);
+    }
+}

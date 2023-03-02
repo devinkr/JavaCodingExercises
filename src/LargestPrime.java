64815e8aa368a040f2276635fd517fffac1df71d
Devin Raleigh <devinkr@gmail.com>
2023-03-02 12:07:39 -0700

    Exercise #26 Largest Prime. Another solution

--- src/LargestPrime.java
+++ src/LargestPrime.java
@@ -11,14 +11,14 @@ public class LargestPrime {
     getLargestPrime (45); should return 5 since 5 is the largest prime (3 * 3 * 5 = 45)
     getLargestPrime (-1); should return -1 since the parameter is negative*/
     public static void main(String[] args) {
-        System.out.println(getLargestPrime (21));
+        System.out.println(getLargestPrime (84));
         System.out.println(getLargestPrime (217));
         System.out.println(getLargestPrime (0));
         System.out.println(getLargestPrime (45));
         System.out.println(getLargestPrime (-1));
     }
 
-    public static int getLargestPrime(int number) {
+/*    public static int getLargestPrime(int number) {
         if (number <= 1) return -1;
         for (int i = number; i >= 2; i--) {
             boolean finishedLoop = true;
@@ -33,5 +33,13 @@ public class LargestPrime {
             }
         }
         return -1;
+    }*/
+
+    public static int getLargestPrime (int number) {
+        if (number<2) return -1;
+        for (int i=number/2; i>1; i--) {
+            if(number%i==0) number=i;
+        }
+        return number;
     }
 }

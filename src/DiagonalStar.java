aac096d6dd30d1fe9ed216b0b07b3f96eb629e7c
Devin Raleigh <devinkr@gmail.com>
2023-03-02 12:21:44 -0700

    Exercise #27 Diagonal Star

--- src/DiagonalStar.java
+++ src/DiagonalStar.java
@@ -55,7 +55,7 @@ public class DiagonalStar {
                     System.out.print(" ");
                 }
             }
-            System.out.println("");
+            System.out.println();
         }
     }
 }
